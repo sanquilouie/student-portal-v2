5860caf529ca294b087bfacdddbb185488a96e44
louieeeee <louiesanqui0518@gmail.com>
2025-02-23 21:25:53 +0800

    Changed file extensions from jsx to tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,60 +1,57 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Toaster } from "react-hot-toast";
+import React from "react";
 
-//Login Imports
-import LoginPage from './pages/login_pages/LoginPage'
-import FacultyLoginPage from './pages/login_pages/FacultyLoginPage'
-import CashierLoginPage from './pages/login_pages/CashierLoginPage'
-import AdminLoginPage from './pages/login_pages/AdminLoginPage'
+// Login Imports
+import LoginPage from './pages/login_pages/LoginPage';
+import FacultyLoginPage from './pages/login_pages/FacultyLoginPage';
+import CashierLoginPage from './pages/login_pages/CashierLoginPage';
+import AdminLoginPage from './pages/login_pages/AdminLoginPage';
 
-//Student Imports
-import HomePage from './pages/student_pages/HomePage'
-import StudentProfilePage from './pages/student_pages/StudentProfilePage'
-import GradesPage from './pages/student_pages/GradesPage'
-import BillingPage from './pages/student_pages/BillingPage'
-import SchedulePage from './pages/student_pages/SchedulePage'
+// Student Imports
+import HomePage from './pages/student_pages/HomePage';
+import StudentProfilePage from './pages/student_pages/StudentProfilePage';
+import GradesPage from './pages/student_pages/GradesPage';
+import BillingPage from './pages/student_pages/BillingPage';
+import SchedulePage from './pages/student_pages/SchedulePage';
 
-//Faculty Imports
-import FacultyHomePage from './pages/faculty_pages/HomePage'
+// Faculty Imports
+import FacultyHomePage from './pages/faculty_pages/HomePage';
 
-//Cashier Imports
-import CashierHomePage from './pages/cashier_pages/HomePage'
+// Cashier Imports
+import CashierHomePage from './pages/cashier_pages/HomePage';
 
-//Admin Imports
-import AdminDashboard from './pages/admin_pages/Dashboard'
-import AdminCashierPage from './pages/admin_pages/users/CashierPage'
-import AdminFacultyPage from './pages/admin_pages/users/FacultyPage'
-import AdminStudentsPage from './pages/admin_pages/users/StudentsPage'
-import UserRegistrationPage from './pages/admin_pages/registration/UserRegistrationPage'
-import StudentRegistrationPage from './pages/admin_pages/registration/StudentRegistrationPage'
-import FacultyRegistrationPage from './pages/admin_pages/registration/FacultyRegistrationPage'
-import CashierRegistrationPage from './pages/admin_pages/registration/CashierRegistrationPage'
+// Admin Imports
+import AdminDashboard from './pages/admin_pages/Dashboard';
+import AdminCashierPage from './pages/admin_pages/users/CashierPage';
+import AdminFacultyPage from './pages/admin_pages/users/FacultyPage';
+import AdminStudentsPage from './pages/admin_pages/users/StudentsPage';
+import UserRegistrationPage from './pages/admin_pages/registration/UserRegistrationPage';
+import StudentRegistrationPage from './pages/admin_pages/registration/StudentRegistrationPage';
+import FacultyRegistrationPage from './pages/admin_pages/registration/FacultyRegistrationPage';
+import CashierRegistrationPage from './pages/admin_pages/registration/CashierRegistrationPage';
 import SubjectsPage from './pages/admin_pages/SubjectsPage';
 import ProgramsPage from './pages/admin_pages/ProgramsPage';
 import DepartmentsPage from './pages/admin_pages/DepartmentsPage';
 
-//Catch-All Imports
+// Catch-All Imports
 import Layout from "./components/Layout";
+import AdminSideBar from "./components/Admin_Sidebar";
 
-import AdminSideBar from "./components/Admin_Sidebar"
-
-function App() {
-
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<LoginPage />} />
-
         <Route path="/faculty" element={<FacultyLoginPage />} />
         <Route path="/faculty/home" element={<FacultyHomePage />} />
-
         <Route path="/cashier" element={<CashierLoginPage />} />
         <Route path="/cashier/home" element={<CashierHomePage />} />
-
         <Route path="/admin" element={<AdminLoginPage />} />
+
         <Route element={<AdminSideBar />}>
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/cashier" element={<AdminCashierPage />} />
@@ -75,13 +72,13 @@ function App() {
           <Route path="/studentbilling" element={<BillingPage />} />
           <Route path="/studentschedule" element={<SchedulePage />} />
         </Route>
+
         <Route path="/studentregistration" element={<StudentRegistrationPage />} />
         <Route path="/userregistration" element={<UserRegistrationPage />} />
       </Routes>
       <ToastContainer />
-    </BrowserRouter>   
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
-
+export default App;
